adbdcb59187329c9ba03d33e93d7a040e8da917b
william <william16.lim@gmail.com>
2024-01-15 00:03:58 +1000

    feat: cleanup files & extract loading step

--- dags/load.py
+++ dags/load.py
@@ -2,8 +2,16 @@ import json
 import pendulum
 from airflow.decorators import dag, task
 from airflow.models import Variable
+import os
+import pandas as pd
+import psycopg2
+import requests
+import base64
 
 streaming_history = Variable.get("streaming_history")
+spotify_id = Variable.get("spotify_id")
+spotify_secret = Variable.get("spotify_secret")
+
 @dag(
     schedule=None,
     start_date=pendulum.datetime(2024, 1, 1, tz="UTC"),
@@ -12,28 +20,74 @@ streaming_history = Variable.get("streaming_history")
 )
 def taskflow_api():
     @task()
-    def extractTransform():
-        order_data_dict = json.loads(streaming_history)
-        return order_data_dict
-
-    @task(multiple_outputs=True)
-    def transform(order_data_dict: dict):
-        total_order_value = 0
-        for value in order_data_dict.values():
-            total_order_value += value
-
-        return {"total_order_value": total_order_value}
+    def extractStreamingHistory():
+      obj = os.scandir(streaming_history)
+      combined_df = pd.DataFrame()
+      for entry in obj:
+          print(entry)
+          if entry.is_file() and entry.name.startswith("StreamingHistory"):
+              df = pd.read_json(entry.path)
+              combined_df = pd.concat([combined_df,df], ignore_index=True)
+          elif entry.is_file() and entry.name.startswith("Marquee"):
+              marquee = pd.read_json(entry.path)
+      combined_df.rename(columns = {'endTime':'end_time', 'trackName': 'track_name', 'msPlayed': 'ms_played', 'artistName':'artist_name'}, inplace = True)
+      combined_df.to_sql('streaming_history', con='postgresql://postgres:postgres@db:5432/spotify', if_exists='replace', 
+                index=False) 
+      if marquee is not None:
+        marquee.rename(columns = {'artistName':'artist_name'}, inplace = True)
+        marquee.to_sql('marquee', con='postgresql://postgres:postgres@db:5432/spotify', if_exists='replace', 
+                  index=False) 
+    
     @task()
-    def load(total_order_value: float):
-        """
-        #### Load task
-        A simple Load task which takes in the result of the Transform task and
-        instead of saving it to end user review, just prints it out.
-        """
+    def extractArtists():
+      connection = psycopg2.connect(user="postgres",
+                                      password="postgres",
+                                      host="db",
+                                      port="5432",
+                                      database="spotify")
+      sql_query = pd.read_sql_query ('''
+                                    SELECT
+                                    *
+                                    FROM streaming_history
+                                    ''', connection)
+      new_df = pd.DataFrame(sql_query, columns = ['artist_name'])
+      artists = new_df.loc[:,['artist_name']].drop_duplicates()
+      artists['image'] = ''
+      artists['id'] = ''
+      artists['url'] = ''
+      artists.reset_index( inplace=True, drop=True)
+      client_id = spotify_id
+      client_secret = spotify_secret
+      encoded = base64.b64encode((client_id + ":" + client_secret).encode("ascii")).decode("ascii")
+      headers = {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "Authorization": "Basic " + encoded
+      }
+      payload = {
+          "grant_type": "client_credentials"
+      }
+      response = requests.post("https://accounts.spotify.com/api/token", data=payload, headers=headers)
+      access_token = response.json()['access_token']
+      for index, row in artists.iterrows():
+          headers = {
+          "Authorization": "Bearer " + access_token
+          }
+          search_params = {
+              'type': 'artist',
+              'q': row['artist_name'],
+          }
+          print(index)
+          response = requests.get("https://api.spotify.com/v1/search", headers=headers, params=search_params)
+          if len(response.json()['artists']['items']) == 0:
+              continue
+          artists.at[index,'id'] = response.json()['artists']['items'][0]['id']
+          if len(response.json()['artists']['items'][0]['images']) != 0:
+              artists.at[index,'image'] = response.json()['artists']['items'][0]['images'][0]['url']
+          artists.at[index,'url'] = response.json()['artists']['items'][0]['external_urls']['spotify']
+          
+      artists.to_sql('artists', con='postgresql://postgres:postgres@db:5432/spotify', if_exists='replace', 
+                index=False) 
 
-        print(f"Total order value is: {total_order_value:.2f}")
-    order_data = extractTransform()
-    order_summary = transform(order_data)
-    load(order_summary["total_order_value"])
+    extractStreamingHistory() >> extractArtists()
 
 taskflow_api()
